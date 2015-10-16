c91c10cb39e76f1b5352f67d08277c7ae34cc072
Kévin Giroux <giroux.kevin.fr@gmail.com>
2015-10-16 16:24:51 +0200

    main

--- Projet2/src/Main.cpp
+++ Projet2/src/Main.cpp
@@ -13,8 +13,6 @@ using namespace std;
 std::string exec(const char* cmd);
 int main()
 {
-	//sf::RenderWindow window;
-	//sf::Clock clk;
 	int fpsCount = 0;
 	int fpsSwitch = 200;
 	bool available = false;
@@ -29,55 +27,7 @@ int main()
 	}
 	string title = "EsigInvaders";
 	GameWindows game(800, 600, title,available);
-	//game.endInit();
 	game.runWindows();
-
-	/*Field field = Field(available);
-	
-	
-	window.create(sf::VideoMode(800, 600), "EsigInvaders");
-	window.setFramerateLimit(60);
-
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
-
-		field.collision(window);
-		field.control();
-
-		if (fpsCount >= fpsSwitch)
-		{
-			field.getSpaceship().switchFps();
-			for (int i = 0; i < field.getBullets().size(); i++)
-			{
-				field.getBullets()[i].switchFps();
-			}
-			fpsCount = 0;
-		}
-		else
-			fpsCount += clk.restart().asMilliseconds();
-
-		window.clear();
-		for (int i = 0; i < field.getBullets().size(); i++)
-		{
-			field.getBullets()[i].move();
-			window.draw(field.getBullets()[i].getSprite());
-		}
-		window.draw(field.getSpaceship().getSprite());
-
-		for (int i = 0; i < field.getEnemies().size(); i++)
-		{
-			field.getEnemies()[i].move();
-			window.draw((field.getEnemies()[i]).getShape());
-		}
-		//cout << "enemi 0 :" << field.getEnemies()[0].getShape().getPosition().x << endl;
-		window.display();
-	}*/
 	return 0;
 }
 
